29a562da5e3cd9d53e0c46d2661c6582d4c08556
David de Klerk <david.deklerk@ed.ac.uk>
2025-01-24 14:33:29 +0000

    Fix run_gunicorn.sh

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -3,7 +3,7 @@
 . app_env
 
 export FLASK_APP=mizzle_server
-export SCRIPT_NAME=/dev/ddekler
+export SCRIPT_NAME=/dev/weatherstation
 
-gunicorn --log-level debug --bind 0.0.0.0:55406 mizzle_server.wsgi:app
+gunicorn --log-level debug --bind 0.0.0.0:55407 mizzle_server.wsgi:app
 
