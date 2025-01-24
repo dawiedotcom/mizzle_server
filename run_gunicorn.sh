d1a6202da07c9e019710ff32140ea545f394b220
David de Klerk <david.deklerk@ed.ac.uk>
2025-01-24 14:14:05 +0000

    Migrate to uv for package management

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-. .venv/bin/activate
 . app_env
 
 export FLASK_APP=mizzle_server
-#export SCRIPT_NAME=/dev/cresh
+export SCRIPT_NAME=/dev/ddekler
 
-gunicorn --log-level debug --bind 0.0.0.0:55405 mizzle_server.wsgi:app
+gunicorn --log-level debug --bind 0.0.0.0:55406 mizzle_server.wsgi:app
 
